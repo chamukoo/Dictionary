24b6b9e963556d2a69283763e13ce968b238424c
Lee Anne <leeangeles2@gmail.com>
2022-11-12 21:53:17 +0800

    Minor adjustments in While statement

--- dictionary.py
+++ dictionary.py
@@ -2,7 +2,7 @@
 
 # DIctionary
 userInfo = {
-    'Lee Anne Angeles' : {'Age':19,'Sex':'Female','Address':'Morong, Rizal','Contact':'09556727785'},
+    'Lee Anne Angeles' : {'Age':19,'Sex':'Female','Address':'Morong, Rizal','Contact':'09556727712'},
 }
 
 def main():
@@ -17,63 +17,71 @@ def main():
         
     # option 1: Ask personal data for contact tracing
     if option == 1:
-        def addUserInfo():
+        while True:
+            print("\n=================== ADD USER INFO ====================")
+            name = input("\n\tEnter your name: ")
+            age = int(input("\tEnter your age: "))
+            sex = input("\tEnter your sex: ")
+            address = input("\tEnter your home address: ")
+            contact = input("\tEnter your contact number: ")
+            print("\n======================================================")
+
+            userInput = {'Age':age,'Sex':sex,'Address':address,'Contact':contact}
+            userInfo[name] = userInput
+
             while True:
-                print("\n=================== ADD USER INFO ====================")
-                name = input("\n\tEnter your name: ")
-                age = int(input("\tEnter your age: "))
-                sex = input("\tEnter your sex: ")
-                address = input("\tEnter your home address: ")
-                contact = input("\tEnter your contact number: ")
-                print("\n======================================================")
-
-                userInput = {'Age':age,'Sex':sex,'Address':address,'Contact':contact}
-                userInfo[name] = userInput
-
-                addAgain = input("Do you want to add another user info? (yes/no): ")
+                addAgain = input("\nDo you want to add another user info? (yes/no): ")
                 if addAgain == "yes":
-                    continue
-                else:
                     break
+                elif addAgain == "no":
+                    main()
+                else:
+                    print("Invalid Input! Please try again.")
+                    continue
                     
 
     # Option 2: Search, ask full name then display the record
     if option == 2:
-        def searchUserName():
+        while True:
+            print("\n================== SEARCH USER NAME ==================")
+            searchName = input("\nWho do you want to search for: ")
+
+            if searchName in userInfo:
+                print("\n\tResult:")
+                print(f"\n\t{'Full Name: ': <15s}", searchName)
+                print(f"\t{'Age: ': <15s}", (userInfo[searchName]['Age']))
+                print(f"\t{'Sex: ': <15s}", (userInfo[searchName]['Sex']))
+                print(f"\t{'Address: ': <15s}", (userInfo[searchName]['Address']))
+                print(f"\t{'Contact: ': <15s}", (userInfo[searchName]['Contact']))
+                print("\n======================================================")
+            else:
+                print("\nNo user is found with the provided name: {}".format(searchName))
+                print("\n======================================================")
+
             while True:
-                print("\n================== SEARCH USER NAME ==================")
-                searchName = input("Who do you want to search for: ")
-
-                if searchName in userInfo:
-                    print("\n\tFull Name: ", searchName, "\n\tAge: ", (userInfo[searchName]['Age']),
-                    "\n\tSex:", (userInfo[searchName]['Sex']), "\n\tAddress: ", (userInfo[searchName]['Address']),
-                    "\n\tContact: ", (userInfo[searchName]['Contact']))
-                else:
-                    print("No user is found with the provided name: {}".format(searchName))
-
-                
-                searchAgain = input("Do you want to search again? (yes/no): ")
+                searchAgain = input("\nDo you want to search again? (yes/no): ")
                 if searchAgain == "yes":
-                    continue
-                else:
-                    print("\n======================================================")
                     break
-            
-    # Option 3: Ask the user if want to exit or retry.
-    if option == 3:
-        def exitProgram():
-            while True:
-                print("======================= EXIT =========================")
-                exitProgram = input("Are you sure you want to exit the program? (yes/no) :")
-
-                if exitProgram == "yes":
-                    print("Thank you for using this program!")
-                    exit()
-                elif exitProgram == "no":
-                    break
+                elif searchAgain == "no":
+                    main()
                 else:
                     print("Invalid Input! Please try again.")
                     continue
+            
+    # Option 3: Ask the user if want to exit or retry.
+    if option == 3:
+        while True:
+            print("======================= EXIT =========================")
+            exitProgram = input("\nDo you want to exit the program? (yes/no) :")
+
+            if exitProgram == "yes":
+                print("Thank you for using this program!")
+                exit()
+            elif exitProgram == "no":
+                main()
+            else:
+                print("Invalid Input! Please try again.")
+                continue
 
 while True:
     main()
